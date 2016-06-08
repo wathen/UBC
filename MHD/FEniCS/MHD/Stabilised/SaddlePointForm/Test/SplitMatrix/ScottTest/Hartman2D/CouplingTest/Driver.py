2961f85d6560db2fca2d84c2e633edca7739dbd3
Michael Wathen <mwathen@cs.ubc.ca>
2016-06-08 14:48:50 -0700

    Updates

--- MHD/FEniCS/MHD/Stabilised/SaddlePointForm/Test/SplitMatrix/ScottTest/Hartman2D/CouplingTest/Driver.py
+++ MHD/FEniCS/MHD/Stabilised/SaddlePointForm/Test/SplitMatrix/ScottTest/Hartman2D/CouplingTest/Driver.py
@@ -9,6 +9,11 @@ import sympy as sy
 import MatrixOperations as MO
 import CheckPetsc4py as CP
 import numpy as np
+import scipy.sparse as sp
+
+def PETSc2Scipy(A):
+    row, col, value = A.getValuesCSR()
+    return sp.csr_matrix((value, col, row), shape=A.size)
 
 def arrayToVec(vecArray):
 
@@ -32,12 +37,21 @@ def Solution():
     uu = x*sy.exp(x+y)
     b = sy.diff(uu,y)
     d = -sy.diff(uu,x)
-
+    # u = y
+    # v = x
+    # b = y
+    # d = x
     NS1 = -d*(sy.diff(d,x)-sy.diff(b,y))
     NS2 = b*(sy.diff(d,x)-sy.diff(b,y))
 
+    print NS1
+    print NS2
+
     M1 = sy.diff(u*d-v*b,y)
     M2 = -sy.diff(u*d-v*b,x)
+
+    print M1
+    print M2
 
     J11 = sy.diff(u, x)
     J12 = sy.diff(u, y)
@@ -48,11 +62,12 @@ def Solution():
     C = Expression((myCCode(M1),myCCode(M2)))
     u0 = Expression((myCCode(u),myCCode(v)))
     b0 = Expression((myCCode(b),myCCode(d)))
+
     Neumann = as_matrix(((Expression(myCCode(J11)), Expression(myCCode(J12))), (Expression(myCCode(J21)), Expression(myCCode(J22)))))
 
     return u0, b0, C, Ct, Neumann
 
-n = int(2**2)
+n = int(2**1)
 
 mesh = UnitSquareMesh(n, n)
 parameters['reorder_dofs_serial'] = False
@@ -90,20 +105,30 @@ bcu = DirichletBC(W.sub(0), u0, boundary)
 bcb = DirichletBC(W.sub(1), b0, boundary)
 bc = [bcu, bcb]
 A = assemble(CoupleT+Couple)
-b = assemble(L_D)
+b_d = assemble(L_D)
+b_n = assemble(L_N)
 for bcs in bc:
     bcs.apply(A)
-    bcs.apply(b)
+    bcs.apply(b_d)
+# print A.array()
+# print b_n.array()
+# print A.array() - b_d.array()
+A, b = CP.Assemble(A, b_n)
+u = arrayToVec(np.concatenate((u0.vector().array(), b0.vector().array()), axis=1))
+print "norm(A*u-f)  ", np.linalg.norm((A*u-b).array)
+print (A*u).array - b.array
 
-A, b = CP.Assemble(A, b)
-
-Ct = A.getSubMatrix(b_is, u_is)
-C = A.getSubMatrix(u_is, b_is)
+C = A.getSubMatrix(b_is, u_is)
+Ct = A.getSubMatrix(u_is, b_is)
 
 f_u = b.getSubVector(u_is)
 f_b = b.getSubVector(b_is)
 
-print "norm(Ct*u-f)  ", np.linalg.norm((Ct*u_solution-f_b).array)
-print "norm(C*b-f):   ", np.linalg.norm((C*b_solution-f_u).array)
-print (Ct*u_solution-f_b).array
-print (C*b_solution-f_u).array
+print "norm(Ct*b-f)  ", np.linalg.norm((Ct*b_solution-f_u).array)
+print "norm(C*u-f):   ", np.linalg.norm((C*u_solution-f_b).array)
+print (Ct*b_solution).array- f_u.array
+print (C*u_solution-f_b).array
+
+# print f_b.array
+
+
