0b8638dd99d400e3ebb1d409971a1c652ef13214
Michael Wathen <mwathen@cs.ubc.ca>
2016-10-05 15:16:46 -0700

    Updates

--- MHD/FEniCS/MHD/Stabilised/SaddlePointForm/Test/SplitMatrix/ScottTest/Hartman2D/MHDprec.py
+++ MHD/FEniCS/MHD/Stabilised/SaddlePointForm/Test/SplitMatrix/ScottTest/Hartman2D/MHDprec.py
@@ -630,3 +630,163 @@ class P(Matrix):
     #     submat = self.P.get
 
 
+
+class ApproxInv(BaseMyPC):
+
+    def __init__(self, W, kspF, kspA, kspQ,Fp,kspScalar, kspCGScalar, kspVector, G, P, A, Hiptmairtol,Options):
+        self.W = W
+        self.kspF = kspF
+        self.kspA = kspA
+        self.kspQ = kspQ
+        self.Fp = Fp
+        self.kspScalar = kspScalar
+        self.kspCGScalar = kspCGScalar
+        self.kspVector = kspVector
+        # self.Bt = Bt
+        self.HiptmairIts = 0
+        self.CGits = 0
+
+
+
+        # print range(self.W[0].dim(),self.W[0].dim()+self.W[1].dim())
+        # ss
+        self.P = P
+        self.G = G
+        self.AA = A
+        self.tol = Hiptmairtol
+        self.u_is = PETSc.IS().createGeneral(range(self.W[0].dim()))
+        self.p_is = PETSc.IS().createGeneral(range(self.W[0].dim(),self.W[0].dim()+self.W[1].dim()))
+        self.b_is = PETSc.IS().createGeneral(range(self.W[0].dim()+self.W[1].dim(),
+            self.W[0].dim()+self.W[1].dim()+self.W[2].dim()))
+        self.r_is = PETSc.IS().createGeneral(range(self.W[0].dim()+self.W[1].dim()+self.W[2].dim(),
+            self.W[0].dim()+self.W[1].dim()+self.W[2].dim()+self.W[3].dim()))
+
+
+
+    def create(self, pc):
+        print "Create"
+
+
+
+    def setUp(self, pc):
+        A, P = pc.getOperators()
+        print A.size
+        if A.type == 'python':
+            self.Ct = A.getPythonContext().getMatrix("Ct")
+            self.Bt = A.getPythonContext().getMatrix("Bt")
+        else:
+            self.C = A.getSubMatrix(self.u_is,self.b_is)
+            self.B = A.getSubMatrix(self.u_is,self.p_is)
+            self.D = A.getSubMatrix(self.b_is,self.r_is)
+        # print self.Ct.view()
+        #CFC = sp.csr_matrix( (data,(row,column)), shape=(self.W[1].dim(),self.W[1].dim()) )
+        #print CFC.shape
+        #CFC = PETSc.Mat().createAIJ(size=CFC.shape,csr=(CFC.indptr, CFC.indices, CFC.data))
+        #print CFC.size, self.AA.size
+        # MO.StoreMatrix(B,"A")
+        # print FC.todense()
+
+        OptDB = PETSc.Options()
+        OptDB["pc_factor_mat_ordering_type"] = "rcm"
+        OptDB["pc_factor_mat_solver_package"] = "mumps"
+
+        self.kspA.setType('preonly')
+        self.kspA.getPC().setType('lu')
+        self.kspA.setFromOptions()
+        self.kspA.setPCSide(0)
+
+        self.kspQ.setType('preonly')
+        self.kspQ.getPC().setType('lu')
+        self.kspQ.setFromOptions()
+        self.kspQ.setPCSide(0)
+
+        self.kspScalar.setType('preonly')
+        self.kspScalar.getPC().setType('lu')
+        self.kspScalar.setFromOptions()
+        self.kspScalar.setPCSide(0)
+
+        kspMX = PETSc.KSP()
+        kspMX.create(comm=PETSc.COMM_WORLD)
+        pcMX = kspMX.getPC()
+        kspMX.setType('preonly')
+        pcMX.setType('lu')
+        OptDB = PETSc.Options()
+        kspMX.setOperators(self.AA,self.AA)
+        self.kspMX = kspMX
+        # self.kspCGScalar.setType('preonly')
+        # self.kspCGScalar.getPC().setType('lu')
+        # self.kspCGScalar.setFromOptions()
+        # self.kspCGScalar.setPCSide(0)
+
+        self.kspVector.setType('preonly')
+        self.kspVector.getPC().setType('lu')
+        self.kspVector.setFromOptions()
+        self.kspVector.setPCSide(0)
+
+
+
+        print "setup"
+    def apply(self, pc, x, y):
+
+        bu = x.getSubVector(self.u_is)
+        xu = bu.duplicate()
+
+        bp = x.getSubVector(self.p_is)
+        xp = bp.duplicate()
+
+        bb = x.getSubVector(self.b_is)
+        xb = bb.duplicate()
+
+        br = x.getSubVector(self.r_is)
+        xr = br.duplicate()
+
+        self.kspF.solve(bu,xu)
+        xp = FluidSchur([kspA, Fp, KspQ], bp)
+        self.kspMX.solve(bb,xb)
+        self.kspScalar.solve(br,xr)
+
+        xp1 = xp.duplicate()
+        self.B.mult(xu, xp1)
+        barF = FluidSchur([kspA, Fp, KspQ], xp1)
+
+        xu1 = xu.duplicate()
+        barS = xu.duplicate()
+        self.B.multTranspose(xp, xu1)
+        self.kspF.solve(xu1, barS)
+
+        xr1 = xr.duplicate()
+        outR = xr.duplicate()
+        self.D.mult(xb, xr1)
+        self.kspScalar(xr1, outR)
+
+        xb1 = xb.duplicate()
+        xb2 = xb.duplicate()
+        xb3 = xb.duplicate()
+        xb4 = xb.duplicate()
+
+        self.D.multTranspose(xr, xb1)
+        self.kspMX.solve(xb1, xb2)
+        self.X.mult(xp, xb3)
+        self.kspMX.solve(xb3, xb4)
+        outB = xb4 + xb + xb2
+
+        xp1 = xu.duplicate()
+        xp2 = xu.duplicate()
+        xp3 = xp.duplicate()
+        self.C.multTranspose(xb, xp1)
+        self.kspF.solve(xp1, xp2)
+        self.B.mult(xp2, xp3)
+        xp4 = FluidSchur([kspA, Fp, KspQ], xp3)
+        outP = barF - xp - xp4;
+
+        xu1 = xu.duplicate()
+        xu2 = xu.duplicate()
+        self.B.multTranspose(barF, xu1)
+        self.kspF.solve(xu1, xu2)
+        outU = xu - xu2 + barS;
+
+
+        y.array = (np.concatenate([outU.array, outP.array, outB.array, outR.array]))
+    def ITS(self):
+        return self.CGits, self.HiptmairIts , self.CGtime, self.HiptmairTime
+
